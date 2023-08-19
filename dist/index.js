7bd21c98b580d5f1ff1c4fbb93bcf6008f41fe1c
Julian871 <vindiesel.1999@mail.ru>
2023-08-19 10:16:48 +0300

    Changes end1

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const settings_1 = require("./settings");
-const port = 3000;
+const port = 5005;
 settings_1.app.listen(port, () => {
     console.log(`Started on ${port} port`);
 });
